bce163629f045b4825e6e008f5a61ca6a8fc7218
DIREK KAKKAR <direkkakkar319@gmail.com>
2025-11-27 23:55:38 +0530

    updated the app.jsx, app.css, main.jsx, index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import { AuthProvider } from './context/AuthContext.jsx'
+import App from './App.jsx'
 
-const root = document.getElementById('root')
-createRoot(root).render(
-  <AuthProvider>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </AuthProvider>
+  </StrictMode>,
 )
